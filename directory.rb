589742ef66813a8f085a532fb68d266399416794
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2018-08-24 13:03:52 +0000

    List count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of villains Academy"
 puts "_ _ _ _ _ _ _ _ _ _"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffery Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
 print "Overall, we have "
+# its importnant that print() doesn't add new line characters
 print 11
 puts " great students"
