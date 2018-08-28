ed1b562ac189111bc77eee84726ebe98d7df4fa9
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2018-08-28 09:28:27 +0000

    Further student info

--- directory.rb
+++ directory.rb
@@ -1,16 +1,22 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array 
+  # create an empty array 
   students = []
-  #get the first name
+  # get the first name
   name = gets.chomp
-  #while the name is not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do 
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
+  # ask for more information about the student
+    puts "Whats #{name}'s favourite hobbie?"
+    hobbies = gets.chomp
+    puts "Country of birth?"
+    birthplace = gets.chomp
+  # add the student hash to the array
+    students << {name: name, cohort: :november, hobbie: hobbies,
+    birthplace: birthplace}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+  # get another name from the user
     name = gets.chomp
   end
   # return the array of students
@@ -25,7 +31,10 @@ end
 def print(students)
   index = 0
   while index <= students.length-1
-    puts "#{index + 1} #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    puts "#{index + 1} #{students[index][:name]}" + 
+    " Main Hobbie: #{students[index][:hobbie]}" + 
+    " Birthplace: #{students[index][:birthplace]}" +
+    " (#{students[index][:cohort]} cohort)"
     index +=1
   end
 end  
