6cca3a721d2ed0a2f3b64f0954c7f837b375ce00
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2018-08-29 12:48:39 +0000

    Save and loading from data file

--- directory.rb
+++ directory.rb
@@ -38,6 +38,8 @@ def print_menu
 # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -49,13 +51,38 @@ def show_students
   print_footer
 end
 
+def save_students
+# open the file for writing
+  file = File.open("students.csv", "w")
+# iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| 
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1" 
 #input students
       input_students
     when "2"
-      show_students     
+      show_students  
+    when "3"
+      save_students
+    when "4"
+      load_students
     when "9"
       exit #will exit the program
     else
@@ -80,5 +107,8 @@ def print_footer
   puts "Overall, we have #{@students.count} great students".center(50)
 end 
 
+#nothing happens until we call interactive_menu
 interactive_menu
-#nothing happens until we call the methods
+
+
+ 
